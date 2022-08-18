490eeddd907bbb2ab8067280022128064662bfbc
Arslan Zaki <59029560+arslanzaki@users.noreply.github.com>
2022-08-18 19:11:08 +0500

    Final Touches

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,18 +1,36 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
+import { add } from "./../store/cartSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchProducts } from "./../store/productSlice"; //THUNK
+import { STATUSES } from "./../store/productSlice";
 
 const Products = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  // const [products, setProducts] = useState([]);
 
-  const fetchProducts = async () => {
-    const api = await fetch("https://fakestoreapi.com/products");
-    const data = await api.json();
-    setProducts(data);
-    console.log(data);
-  };
+  const { data: products, status } = useSelector((state) => state.product);
+
+  // const fetchProducts = async () => {
+  //   const api = await fetch("https://fakestoreapi.com/products");
+  //   const data = await api.json();
+  //   setProducts(data);
+  //   console.log(data);
+  // };
 
   useEffect(() => {
-    fetchProducts();
+    // fetchProducts();
+    dispatch(fetchProducts());
   }, []);
+
+  function handleAdd(product) {
+    dispatch(add(product));
+  }
+
+  if (status === STATUSES.LOADING) {
+    return <h2>Loading...</h2>;
+  } else if (status === STATUSES.ERROR) {
+    return <h2>Something Went Wrong...</h2>
+  }
   return (
     <div className="productsWrapper">
       {products.map((product) => (
@@ -20,7 +38,9 @@ const Products = () => {
           <img src={product.image} alt="product" />
           <h4>{product.title}</h4>
           <h5>{product.price}</h5>
-          <button className="btn">Add To Cart</button>
+          <button onClick={() => handleAdd(product)} className="btn">
+            Add To Cart
+          </button>
         </div>
       ))}
     </div>
---
+++ src/store/productSlice.js
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export const STATUSES = Object.freeze({
+  IDLE: "idle",
+  ERROR: "error",
+  LOADING: "loading",
+});
+
+const productSlice = createSlice({
+  name: "product",
+  initialState: {
+    data: [],
+    status: STATUSES.IDLE,
+  },
+  reducers: {
+    // setProducts(state, action) {
+    //   state.data = action.payload;
+    // },
+    // setStatus(state, action) {
+    //   state.status = action.payload;
+    // },
+  },
+  // FOR ANOTHER WAY THUNKS
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchProducts.pending, (state, action) => {
+        state.status = STATUSES.LOADING;
+      })
+      .addCase(fetchProducts.fulfilled, (state, action) => {
+        state.data = action.payload;
+        state.status = STATUSES.IDLE;
+      })
+      .addCase(fetchProducts.rejected, (state, action) => {
+        state.status = STATUSES.ERROR;
+      });
+  },
+});
+
+export const { setProducts, setStatus } = productSlice.actions;
+export default productSlice.reducer;
+
+//THUNKS
+
+// export function fetchProducts() {
+//   return async function fetchProductsThunk(dispatch, getState) {
+//     dispatch(setStatus(STATUSES.LOADING));
+//     try {
+//       const api = await fetch("https://fakestoreapi.com/products");
+//       const data = await api.json();
+//       dispatch(setProducts(data));
+//       dispatch(setStatus(STATUSES.IDLE));
+//     } catch (err) {
+//       console.log(err);
+//       dispatch(setStatus(STATUSES.ERROR));
+//     }
+//   };
+// }
+
+// ANOTHER WAY FOR THUNKS [For Better Error Handling]
+
+export const fetchProducts = createAsyncThunk("products/fetch", async () => {
+  const api = await fetch("https://fakestoreapi.com/products");
+  const data = await api.json();
+  return data;
+});
